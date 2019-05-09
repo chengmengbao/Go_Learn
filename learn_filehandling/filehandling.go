97597b5acb398d5e61e26c8c8c020500c8a5fca2
chengmengbao <paul_chengmengbao@163.com>
2019-05-09 21:17:09 +0800

    commit filehandling.go

--- learn_filehandling/filehandling.go
+++ learn_filehandling/filehandling.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		fmt.Println("文件读取错误：", err)
+		return
+	}
+	fmt.Println("1 文件的内容：", data)
+	fmt.Println("2 文件的内容：", string(data))
+}
